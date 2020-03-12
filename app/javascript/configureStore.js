591ffaf4f8b74a25e9bedf8148279043b1973b6f
grace-mugoiri <gracemugoiri@gmail.com>
2020-03-12 14:16:35 +0300

    add ul in HelloWorld that reads things from redux state

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,7 +1,12 @@
 import { createStore } from 'redux';
 
 const initialState = {
-	things: []
+	things: [
+		{
+			name: "test",
+			guid: "73773"
+		}
+	]
 };
 
 function rootReducer(state, action) {
